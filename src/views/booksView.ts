c6ea68604731530e21ff7db2c66b648c62771a29
Nicolás Martín Rojo <nicolas.martin.rojo@gmail.com>
2026-02-05 21:00:25 -0300

    add MetricsResponse

--- src/views/booksView.ts
+++ src/views/booksView.ts
@@ -5,10 +5,18 @@ interface GetBooksQuery {
   author?: string
 }
 
+type MetricsResponse = {
+  mean_units_sold: number;
+  cheapest_book: number;
+  books_written_by_author: number;
+};
 export const booksView = (booksController: BooksController) => {
   return {
-    getBooks: async (req: Request<{}, {}, {}, GetBooksQuery>, res: Response<any>) => {
-      await booksController.getBooks(req, res)
-    }
-  }
-}
+    getBooks: async (
+      req: Request<{}, {}, {}, GetBooksQuery>,
+      res: Response<Promise<MetricsResponse>>,
+    ) => {
+      await booksController.getBooks(req, res);
+    },
+  };
+};
